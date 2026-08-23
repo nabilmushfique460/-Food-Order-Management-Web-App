7b7d75ae0efd3f6d1495e496ef968a27b3fa07c0
NABIL <nabilmushfique460@gmail.com>
2026-08-24 01:36:39 +0600

    Set up and connect sqlite database

--- restaurant_menu/models.py
+++ restaurant_menu/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+MEAL_TYPE = (
+    ("starter", "Stater"),
+    ("salads", "Salads"),
+    ("main_dishes", "Main Dishes"),
+    ("desserts", "Desserts")
+)
+
+STATUS = (
+    (0, "Unavailable"),
+    (1, "Available"),
+)
+
+class Items(models.Model):
+    meal = models.CharField(max_length=1000, unique=True)
+    description = models.CharField(max_length=2000)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=200, choices=MEAL_TYPE)
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    status = models.IntegerField(choices=STATUS, default=0)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.meal
